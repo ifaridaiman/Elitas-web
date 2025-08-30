ceaeaefb7f2eeed496219079173e5e599b37d7d0
Red <ifaridaiman@gmail.com>
2025-08-30 21:52:41 +0800

    update content

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -2,7 +2,7 @@ import HeaderPage from "@/components/ui/HeaderPage";
 import React from "react";
 import HistoryMission from "./_partials/HistoryMission";
 import OurValues from "./_partials/OurValues";
-import { values, doctors,whychooseus } from "@/contents/about-us.json";
+import { title, description, values, doctors,whychooseus, supportiveStaffDescription } from "@/contents/about-us.json";
 import { Heart, Users, Lightbulb } from "lucide-react";
 import MeetDoctor from "./_partials/MeetDoctor";
 import ChooseUs from "./_partials/ChooseUs";
@@ -12,13 +12,13 @@ const AboutUs = () => {
   return (
     <>
       <HeaderPage
-        title="About Us"
-        description="Dedicated to providing exceptional healthcare services with a patient-first approach, modern facilities, and experienced medical professionals who care about your wellbeing."
+        title={title}
+        description={description}
       />
       <HistoryMission />
       <OurValues values={values} />
       <MeetDoctor doctors={doctors} />
-      <SupportiveStaff />
+      <SupportiveStaff description={supportiveStaffDescription} />
       <ChooseUs whyChooseUs={whychooseus} />
     </>
   );
